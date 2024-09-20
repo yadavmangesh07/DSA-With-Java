9b20bd84fd24a7a57e8e12df3ef391d0b6abf926
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-09-20 23:22:02 +0530

    Refactor kadanesAlgo.java for improved readability and maintainability Add subsequenceRecursion.java for generating subsequences

--- Algorithms/kadanesAlgo.java
+++ Algorithms/kadanesAlgo.java
@@ -1,23 +1,25 @@
 public class kadanesAlgo {
-    public static void kadanes(int arr[]){
-        int currentsum=0;
-        int maxsum=Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            currentsum+=arr[i];
-            
-            if(currentsum<0){
-                currentsum=0;
-            }
-            maxsum=Math.max(maxsum, currentsum);
-
-            
-        }
-        System.out.println("max sum :"+maxsum);
-    }
-    public static void main(String[] args) {
-        int array[]={-2,-3,4,-1,-2,1,5,-3};
-        kadanes(array);
+    public static void kadanes(int arr[]) {
+        int currentsum = 0;
+        int maxsum = Integer.MIN_VALUE;
         
+        for (int i = 0; i < arr.length; i++) {
+            currentsum += arr[i];
+            
+            if (currentsum > maxsum) {
+                maxsum = currentsum;
+            }
+            
+            if (currentsum < 0) {
+                currentsum = 0;
+            }
+        }
+        
+        System.out.println("max sum: " + maxsum);
     }
-    
+
+    public static void main(String[] args) {
+        int array[] = {-1, -2, -3, -4};
+        kadanes(array);
+    }
 }
---
+++ LeetCode/subsequenceRecursion.java
@@ -0,0 +1,46 @@
+package LeetCode;
+
+import java.util.ArrayList;
+
+public class subsequenceRecursion {
+    public static ArrayList<ArrayList<Integer>> subsequence(ArrayList<Integer> ls){
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        ArrayList<Integer> res2=new ArrayList<>();
+        helper(res, ls, 0, res2);
+        for (ArrayList<Integer> list : res) {
+            if(list.size()==2){
+                System.out.println(list);
+            }
+            
+        }
+
+        return res;
+    }
+    public static void helper(ArrayList<ArrayList<Integer>> rs,ArrayList<Integer> ls,int s,ArrayList<Integer> ss){
+        if(s==ls.size()){
+             rs.add(new ArrayList<>(ss));
+             return;
+            
+        }
+        ss.add(ls.get(s));
+        helper(rs, ls, s+1, ss);
+        ss.remove(ls.get(s));
+        helper(rs, ls, s+1, ss);
+
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> ls=new ArrayList<>();
+        ls.add(1);
+        ls.add(2);
+        ls.add(1);
+        ls.add(3);
+        ls.add(2);
+        
+        System.out.println(subsequence(ls));
+    
+        
+        
+    }
+    
+}
