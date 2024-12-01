51378af223b1f073a1e74e6137cf8758d7cad88b
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-12-01 18:27:47 +0530

    Refactor Lc_1346.java to simplify logic for checking if double exists in an array

--- LeetCode/Lc_1346.java
+++ LeetCode/Lc_1346.java
@@ -9,23 +9,21 @@ public class Lc_1346 {
             int a=arr[i];
             hm.put(a,hm.getOrDefault(a,0)+1);
         }
-        int count=0;
         for(int i=0;i<arr.length;i++){
             int a=2*arr[i];
-            System.out.println(arr[i]);
             if(a!=0){
                 if(hm.containsKey(a)){
-                    count++;
+                   return true;
                 }
 
             }
             else{
                 if(hm.get(a)>1){
-                    count++;
+                   return true;
                 }
             }
         }
-        return count>=1?true:false;
+        return false;
         
     }
     public static void main(String[] args) {
